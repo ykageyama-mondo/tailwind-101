58add83165d51156c361607e9c53b775b6e098a9
Yuichi <yuichi.kageyama@mondo.com.au>
2023-05-17 14:56:07 +1000

    Hands On
    Co-authored-by: Fraser <fmac123@users.noreply.github.com>
    Co-authored-by: charliehart-mondo <charliehart-mondo@users.noreply.github.com>
    Co-authored-by: Amin Fazl <AminFazlMondo@users.noreply.github.com>
    Co-authored-by: Scott McFarlane <scottmondo@users.noreply.github.com>
    Co-authored-by: Kevin <MondoKevin@users.noreply.github.com>
    Co-authored-by: MicahRus <MicahRus@users.noreply.github.com>

--- src/topics/3.HandsOn.tsx
+++ src/topics/3.HandsOn.tsx
@@ -1,47 +1,67 @@
-import {Sandpack} from '@codesandbox/sandpack-react'
-import {Details} from '../helpers/details'
+import {useState} from 'react'
 import {Topic} from '../helpers/topic'
 
 export const HandsOn = () => {
+  const [darkMode, setDarkMode] = useState(false)
+
+  const [yesCount, setYesCount] = useState(0)
+  const [noCount, setNoCount] = useState(0)
+
   return (
     <Topic title='Hands On with Tailwind.'>
-      <div>
+      <div className={darkMode ? 'dark' : ''}>
         <p className='py-2'>Let's try to build a simple page with Tailwind.</p>
         <div>
-          <p>I want this text to be big and italic</p>
-          <p>I want this text to be small and underlined</p>
+          {/* https://tailwindcss.com/docs/font-size */}
+          {/* https://tailwindcss.com/docs/text-decoration */}
+          <p className='italic text-9xl'>I want this text to be big and italic</p>
+          <p className='text-xs underline'>I want this text to be small and underlined</p>
         </div>
 
         <div>
-          <div className='bg-rose-500'>I want this to be on the right</div>
-          <div className='bg-teal-500'>I want this to be on the left</div>
+          <div className='bg-rose-500 text-right px-1'>I want this to be on the right</div>
+          <div className='bg-teal-500 text-left px-1'>I want this to be on the left</div>
         </div>
 
-        <div>
-          I want this to be half height when on mobile, and full height when on desktop
+        <div className='bg-rose-100 w-full sm:w-1/2'>
+          Full width when on mobile, and half width when on desktop
         </div>
 
-        <div>
+        <div className="flex justify-between gap-4 items-center">
+          <button onClick={() => setDarkMode(!darkMode)}>{darkMode ? '🌙' : '🌞'}</button>
+          <p className='flex-grow bg-white dark:bg-black dark:text-white'>
           I want this to be dark on dark mode, and light on light mode
+          </p>
         </div>
 
-        <div>
+        <div className="hover:bg-red-700">
           I want this to do something when I hover
         </div>
 
         <div className=''>
           I don't like the indigo default colours, so I want to change them
-          <div className='p-2 bg-indigo-100'></div>
-          <div className='p-2 bg-indigo-200'></div>
-          <div className='p-2 bg-indigo-300'></div>
-          <div className='p-2 bg-indigo-400'></div>
-          <div className='p-2 bg-indigo-500'></div>
-          <div className='p-2 bg-indigo-600'></div>
-          <div className='p-2 bg-indigo-700'></div>
-          <div className='p-2 bg-indigo-800'></div>
-          <div className='p-2 bg-indigo-900'></div>
+          <div className='p-2 bg-green-100'></div>
+          <div className='p-2 bg-green-200'></div>
+          <div className='p-2 bg-green-300'></div>
+          <div className='p-2 bg-green-400'></div>
+          <div className='p-2 bg-green-500'></div>
+          <div className='p-2 bg-green-600'></div>
+          <div className='p-2 bg-green-700'></div>
+          <div className='p-2 bg-green-800'></div>
+          <div className='p-2 bg-green-900'></div>
         </div>
       </div>
+
+      <div className='uppercase'>i want this to be all upper case</div>
+      <div className='capitalize underline decoration-wavy'>i want this to be capatilized.</div>
+      <div className="pt-6">
+      <h1>Do we want to try Tailwind??</h1>
+      <div className="flex ">
+        <button className="bg-emerald-100" onClick={() => setYesCount(yesCount + 1)}>YES {yesCount}</button>
+        <button className="bg-pink-100" onClick={() => setNoCount(noCount + 1)}>NO {noCount}</button>
+      </div>
+      </div>
+
     </Topic>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{html,tsx}"],
+  darkMode: 'class',
   theme: {
-    extend: {
-      transitionProperty: {
-        'height': 'height'
+      colors: {
+        ...colors,
+        green: {
+          100: '#222222',
+          200: '#0f0af5',
+          300: '#afafaf',
+          400: '#12aa23',
+          500: '#aaaaaa',
+          600: '#008800',
+          700: '#ffffff',
+          800: '#000000',
+          900: '#00ff00'
+        },
+        indigo: {
+          100: '#222222',
+          200: '#0f0af5',
+          300: '#afafaf',
+          400: '#12aa23',
+          500: '#aaaaaa',
+          600: '#008800',
+          700: '#ffffff',
+          800: '#000000',
+          900: '#00ff00'
+        }
       }
-    },
   },
   plugins: [],
 }
